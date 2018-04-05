b62d750b7299cd07bfdffda3ee524bc548ebf36d
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-04-05 22:26:42 +0200

    as & rs service

---
+++ oauth-as/src/main/java/com/lifeinide/oauth2/service/as/OAuthASApplication.java
@@ -0,0 +1,18 @@
+package com.lifeinide.oauth2.service.as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * External oauth2 client example.
+ *
+ * @author Lukasz Frankowski (lifeinide.com)
+ */
+@SpringBootApplication
+public class OAuthASApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthASApplication.class, args);
+	}
+
+}
---
+++ oauth-rs/src/main/java/com/lifeinide/oauth2/service/rs/OAuthRSApplication.java
@@ -0,0 +1,22 @@
+package com.lifeinide.oauth2.service.rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * External oauth2 client example.
+ *
+ * @author Lukasz Frankowski (lifeinide.com)
+ */
+@SpringBootApplication
+@EnableResourceServer
+@EnableWebSecurity
+public class OAuthRSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthRSApplication.class, args);
+	}
+
+}
