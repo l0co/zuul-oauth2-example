29bb213717938a97aeedae0d5bf840c2fbe280c7
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2018-04-14 18:22:49 +0200

    disable web security

--- oauth-as/src/main/java/com/lifeinide/oauth2/service/as/OAuthASApplication.java
+++ oauth-as/src/main/java/com/lifeinide/oauth2/service/as/OAuthASApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -32,7 +31,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @SpringBootApplication
 @EnableAuthorizationServer
 @EnableResourceServer
-@EnableWebSecurity
 public class OAuthASApplication implements AuthorizationServerConfigurer, ResourceServerConfigurer {
 
 	/**********************************************************************************************************
